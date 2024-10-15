8ed0952e7d40fdd485b9df0b5cb3f30b84fa5e4a
Bevis-Chew-Kian-Hong <163868142+Bevis-Chew-Kian-Hong@users.noreply.github.com>
2024-10-15 09:26:18 +0800

    Update question_6_reverse_string.py 1

--- question_6_reverse_string.py
+++ question_6_reverse_string.py
@@ -21,7 +21,8 @@ def string_reverse(str1):
     return rstr1
 
 def main():    
-    print("String reverse for \'12345abcde\' is", string_reverse(str1))
+    str1 = '12345abcde'
+    print("String reverse for \'{}\' is \'{}\'".format(str1,string_reverse(str1)))
 
 # Don't change the code below!
 if __name__ == "__main__":
